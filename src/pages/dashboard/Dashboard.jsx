6b97ab92537b9d4f2ef3593ca60391e22c60b8c9
Nico Gasco <nikolasgv@gmail.com>
2023-08-29 16:04:37 -0300

    Branch TP1 Completed

---
+++ src/pages/dashboard/Dashboard.jsx
@@ -0,0 +1,33 @@
+import React, {useEffect} from "react";
+import {
+    Grid,
+    Paper,
+    Box
+} from '@mui/material';
+
+
+const Dashboard = () => {
+
+    return (
+    <Grid container spacing={3}>
+        <Grid item xs={12} >
+            <Paper sx={{ p: 1, backgroundColor: '#B5FF86', border: '4px dashed #000' }}>
+                <Box sx={{ m: 1 }}>
+                    Todo
+                </Box>
+            </Paper>
+        </Grid>
+
+        <Grid item xs={12}>
+            <Paper sx={{ p: 1, backgroundColor: '#B5FF86', border: '4px dashed #000' }}>
+                <Box sx={{ m: 1 }}>
+                    FetchList
+                </Box>
+            </Paper>
+        </Grid>
+    </Grid>
+    
+    );
+};
+
+export default Dashboard;
---
+++ src/pages/todo/Todo.js
@@ -0,0 +1,47 @@
+import React, {useEffect} from "react";
+import {
+    Grid,
+    Paper,
+    Box
+} from '@mui/material';
+
+/*
+
+import { appSelector } from "../../redux/appRedux";
+import { useSelector } from 'react-redux';
+
+
+
+    const todoList = useSelector(appSelector.todo);
+
+    const totalTareas = todoList.length;
+
+    // Tareas completadas
+    const tareasCompletadas = todoList.filter(task => task.completed);
+  
+    // Cantidad de tareas completadas
+    const tareasCompletadasCount = tareasCompletadas.length;
+
+    // Tareas pendientes
+    const tareasPendientes = todoList.filter(task => !task.completed);
+
+    // Cantidad de tareas pendientes
+    const tareasPendientesCount = tareasPendientes.length;
+*/
+const Todo = () => {
+
+    return (
+    <Grid container spacing={3}>
+        <Grid item xs={12} >
+            <Paper sx={{ p: 1, backgroundColor: '#B5FF86', border: '4px dashed #000' }}>
+                <Box sx={{ m: 1 }}>
+                    Todo
+                </Box>
+            </Paper>
+        </Grid>
+    </Grid>
+    
+    );
+};
+
+export default Todo;
